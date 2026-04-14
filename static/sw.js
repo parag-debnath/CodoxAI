bdff3ab5ef09fb1a72c7298696b5d317cc603d18
Parag Debnath <94636114+parag-debnath@users.noreply.github.com>
2026-04-14 11:08:39 +0530

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,18 @@
+var CACHE = 'codoxai-v1';
+var FILES = ['/'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request);
+    })
+  );
+});
